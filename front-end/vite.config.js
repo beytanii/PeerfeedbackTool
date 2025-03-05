e1c1b672de07205454dd4d5beb31935563f99751
Bethany <bc5277@rit.edu>
2025-03-04 21:40:20 -0500

    change vite.config.js

--- front-end/vite.config.js
+++ front-end/vite.config.js
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    host: true, // Allows external access
+    allowedHosts: ['peerfeedback.betbet.website'],
+    port: 5173,
+  },
   plugins: [react()],
 })
