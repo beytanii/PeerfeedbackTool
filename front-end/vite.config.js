3aad0c60fa613d2b4f0f2a4ab2e0793d1f9ce64d
Bethany <bc5277@rit.edu>
2025-03-05 00:57:51 -0500

    vite changes

--- front-end/vite.config.js
+++ front-end/vite.config.js
@@ -7,10 +7,6 @@ export default defineConfig({
     host: true, // Allows external access
     allowedHosts: ['peerfeedback.betbet.website'],
     port: 5173,
-    hmr: process.env.NODE_ENV !== 'production' ? true : false, // Disable HMR in production
-  },
-  preview: {
-    hmr: false, // Disable WebSockets when using 'npm run preview'
   },
   plugins: [react()],
 })
