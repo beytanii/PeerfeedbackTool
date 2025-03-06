fd6af4607f38bd226a004a226ff6bab43045fe4e
Bethany <bc5277@rit.edu>
2025-03-05 19:41:23 -0500

    add new user

--- back-end/services/connectToDB.js
+++ back-end/services/connectToDB.js
@@ -3,8 +3,8 @@ const mysql = require('mysql2');
 // Create a pool of connections to the database
 const connection = mysql.createPool({
     host: 'peerfeedback.betbet.website',      // Replace with your host
-    user: 'root',           // Replace with your MySQL username
-    password: 'betbet',    // Replace with your MySQL password
+    user: 'admin',           // Replace with your MySQL username
+    password: 'peeradmin',    // Replace with your MySQL password
     database: 'PeerFeedback' // Replace with the name of your database
 });
 
